bc7e91b8ac3f4212f3c3765b36d718af08178d30
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-21 13:30:22 +0530

    first pattern added

---
+++ src/main/java/coreconcepts/oops/PatternOne.java
@@ -0,0 +1,29 @@
+package coreconcepts.oops;
+
+import java.util.Scanner;
+
+public class PatternOne {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter your number");
+        int n=scanner.nextInt();
+
+        for (int i=0;i<n;i++)
+        {
+            for (int j=0;j<n;j++)
+            {
+                if (i>=j)
+                {
+                    System.out.print("*");
+                }
+
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
