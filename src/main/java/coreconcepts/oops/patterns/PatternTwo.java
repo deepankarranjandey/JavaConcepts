46b440395babac570d6093e19c2b114fccc08892
deepankarranjandey <deepankarranjandey@gmail.com>
2024-01-23 13:05:29 +0530

    struct changes3

---
+++ src/main/java/coreconcepts/oops/patterns/PatternTwo.java
@@ -0,0 +1,21 @@
+package coreconcepts.oops.patterns;
+
+public class PatternTwo {
+
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i <= (n - 1) / 2 || i == j) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+        }
+    }
+
+}
